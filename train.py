3ba71046eeeb7211957ae3cc6c7ead8dfaf5c73f
n28div <nicolaslazzari98@gmail.com>
2022-12-20 13:49:46 +0100

    training script

--- train.py
+++ train.py
@@ -6,65 +6,103 @@ import pytorch_lightning as pl
 
 from choco.corpus import ChoCoHarteAnnotationsCorpus
 from pitchclass2vec.data import ChocoDataModule
-from pitchclass2vec.rootinterval import RootIntervalModel, RootIntervalDataset
+import pitchclass2vec.encoding as encoding
+import pitchclass2vec.model as model
 
 
 ENCODING_MAP = {
-  "root-interval": (RootIntervalModel, RootIntervalDataset)
+    "root-interval": encoding.RootIntervalDataset,
+    "all-interval": encoding.AllIntervalDataset,
+    "chord2vec": encoding.Chord2vecDataset
 }
 
-parser = argparse.ArgumentParser(description="Train pitchclass2vec embedding.")
-parser.add_argument("--choco", type=str, required=True)
-parser.add_argument("--out", type=str, required=True)
-parser.add_argument("--encoding", choices=["root-interval",], required=True, default="root-interval")
-parser.add_argument("--seed", type=int, required=False, default=42)
-parser.add_argument("--max-epochs", type=int, required=False, default=5)
-parser.add_argument("--batch-size", type=int, required=False, default=512)
-parser.add_argument("--embedding-dim", type=int, required=False, default=10)
-parser.add_argument("--embedding-aggr", choices=["sum", "mean"], required=False, default="sum")
-parser.add_argument("--context", type=int, required=False, default=5)
-parser.add_argument("--negative-sampling-k", type=int, required=False, default=20)
-parser.add_argument("--early-stop-patience", type=int, required=False, default=2)
+MODEL_MAP = {
+    "word2vec": model.Word2vecModel,
+    "fasttext": model.FasttextModel
+}
 
-args = parser.parse_args()
+
+def train(choco: str, encoding: str, model: str, out: str, seed: int = 42, batch_size: int = 32, context: int = 5,
+          negative_sampling_k: int = 20, embedding_dim: int = 10, embedding_aggr: str = "sum",
+          early_stop_patience: int = 2, disable_wandb: bool = False, max_epochs: int = 10):
+    pl.seed_everything(seed=seed, workers=True)
+
+    dataset_cls = ENCODING_MAP[encoding]
+    model_cls = MODEL_MAP[model]
+
+    data = ChocoDataModule(
+        choco,
+        dataset_cls,
+        batch_size=batch_size,
+        context_size=context,
+        negative_sampling_k=negative_sampling_k)
+
+    model = model_cls(embedding_dim=embedding_dim,
+                      aggr=embedding_aggr)
+
+    callbacks = [
+        pl.callbacks.EarlyStopping(monitor="train/loss",
+                                   min_delta=0.00,
+                                   patience=early_stop_patience),
+        pl.callbacks.ModelCheckpoint(save_top_k=1,
+                                     monitor="train/loss",
+                                     mode="min",
+                                     dirpath=out,
+                                     filename="{epoch}",
+                                     every_n_epochs=1)
+    ]
+
+    if not disable_wandb:
+        logger = pl.loggers.WandbLogger(project="pitch2vec",
+                                        group=f"{encoding}_{model}",
+                                        tags=[
+                                            f"{embedding_dim} embedding",
+                                            f"{embedding_aggr} aggregation",
+                                            f"{context} context",
+                                            f"{negative_sampling_k} negative sampling k"
+                                        ])
+        callbacks.append(pl.callbacks.LearningRateMonitor(logging_interval="step"))
+    else:
+        logger = None
+
+    trainer = pl.Trainer(max_epochs=max_epochs,
+                         accelerator="auto",
+                         logger=None if disable_wandb else wandb_logger,
+                         devices=1,
+                         callbacks=callbacks)
+
+    trainer.fit(model, datamodule=data)
+
 
 if __name__ == "__main__":
-  pl.seed_everything(seed=args.seed, workers=True)
+    parser = argparse.ArgumentParser(description="Train pitchclass2vec embedding.")
+    parser.add_argument("--choco", type=str, required=True)
+    parser.add_argument("--out", type=str, required=True)
+    parser.add_argument("--encoding", 
+                        choices=list(ENCODING_MAP.keys()), 
+                        required=True, 
+                        default="root-interval")
+    parser.add_argument("--model", 
+                        choices=list(MODEL_MAP.keys()), 
+                        required=True, 
+                        default="fasttext")
+    parser.add_argument("--seed", type=int, required=False, default=42)
+    parser.add_argument("--max-epochs", type=int, required=False, default=5)
+    parser.add_argument("--batch-size", type=int, required=False, default=512)
+    parser.add_argument("--embedding-dim", type=int, required=False, default=10)
+    parser.add_argument("--embedding-aggr",
+                        choices=["sum", "mean"], required=False, default="sum")
+    parser.add_argument("--context", type=int, required=False, default=5)
+    parser.add_argument("--negative-sampling-k", type=int,
+                        required=False, default=20)
+    parser.add_argument("--early-stop-patience", type=int,
+                        required=False, default=2)
+    parser.add_argument("--disable-wandb", action='store_const',
+                        const=True, default=False)
 
-  model_cls, dataset_cls = ENCODING_MAP[args.encoding]
-  
-  data = ChocoDataModule(
-    args.choco, 
-    dataset_cls, 
-    batch_size=args.batch_size, 
-    context_size=args.context,
-    negative_sampling_k=args.negative_sampling_k)
-  
-  model = model_cls(embedding_dim=args.embedding_dim, aggr=args.embedding_aggr)
-
-  wandb_group = args.encoding
-  wandb_tags = [
-    f"{args.embedding_dim} embedding",
-    f"{args.embedding_aggr} aggregation",
-    f"{args.context} context",
-    f"{args.negative_sampling_k} negative sampling k"
-  ]
-    
-  trainer = pl.Trainer(max_epochs=args.max_epochs, 
-                       accelerator="auto",
-                       logger=pl.loggers.WandbLogger(project="pitch2vec", group=wandb_group, tags=wandb_tags),
-                       devices=1,
-                       callbacks=[
-                         pl.callbacks.LearningRateMonitor(logging_interval="step"),
-                         pl.callbacks.EarlyStopping(monitor="train/loss", 
-                                                    min_delta=0.00, 
-                                                    patience=args.early_stop_patience),
-                         pl.callbacks.ModelCheckpoint(save_top_k=1, 
-                                                      monitor="train/loss", 
-                                                      mode="min", 
-                                                      dirpath=args.out, 
-                                                      filename="{epoch}",
-                                                      every_n_epochs=1)
-                        ])
-  
-  trainer.fit(model, datamodule=data)
+    args = parser.parse_args()
+    train(args.choco, args.encoding, args.model, args.out, seed=args.seed, 
+          batch_size=args.batch_size, context=args.context, 
+          negative_sampling_k=args.negative_sampling_k, embedding_dim=args.embedding_dim, 
+          embedding_aggr=args.embedding_aggr, early_stop_patience=args.early_stop_patience, 
+          disable_wandb=args.disable_wandb, max_epochs=args.max_epochs)
